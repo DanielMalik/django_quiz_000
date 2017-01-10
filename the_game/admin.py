df25a0e172fd05b2adec177c51425f9515a03c0f
Daniel Malik <daniel.malik.malik@gmail.com>
2017-01-10 20:43:12 +0100

    class Question and class Category, admin reg, migrated

--- the_game/admin.py
+++ the_game/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from the_game.models import Question, Category
 
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("category", "grade", "answer", "query", "comment")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("cat_name", )
--- the_game/models.py
+++ the_game/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+GRADE = (
+    (0, 'easy'),
+    (1, 'normal'),
+    (2, 'hard'),
+    (3, 'unfair')
+)
+
+class Question(models.Model):
+    query = models.TextField(max_length=512)
+    answer = models.CharField(max_length=128,)
+    comment = models.TextField(max_length=512, null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    grade = models.IntegerField(choices=GRADE, default=0)
+
+    def __str__(self):
+        return self.query[0:30]
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=128,)
+
+    def __str__(self):
+        return self.cat_name
